c8c7ceeb5520668c2e2025594e26cc240b5e99ad
Andy Fang <afang1094@gmail.com>
2021-10-12 15:15:04 -0700

    installed core dependencies

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(PORT, () => console.log(`Listening at localhost:${PORT}!`));
