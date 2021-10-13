8f9377e4bfa452ddd24105f93c90ec36fd4865d4
Andy Fang <afang1094@gmail.com>
2021-10-12 18:07:32 -0700

    added NavBar and some styling

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import axios from 'axios';
+import Context from './Context.jsx';
+import NavBar from './NavBar.jsx';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <NavBar />
+    </Router>
   )
 };
 
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Nav, NavContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavElements.jsx';
+import { FaBars } from 'react-icons/fa'
+
+const NavBar = () => {
+  return (
+    <>
+      <Nav>
+        <NavContainer>
+          <NavLogo to='/'>trippy</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to='about'>About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='discover'>Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='services'>Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='signup'>Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+          </NavBtn>
+        </NavContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default NavBar;
